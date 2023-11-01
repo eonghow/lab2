978bb20878b2f7885390aaa828879880b3a8ed07
eonghow <eonghow.21@ichat.sp.edu.sg>
2023-11-01 11:42:55 +0800

    test files

--- Lab2.py
+++ Lab2.py
@@ -1,32 +1,38 @@
 print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def main():
+    print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+    display_main_menu()
+    calc_average(get_user_input())
+    calc_min_max(get_user_input())
+    medianlist(get_user_input())
 
 def display_main_menu():
-    print("Enter some numbers separated by commas (e.g. 5, 67, 32)")
+    print("display_main_menu")
+def calc_average(list):
+    avg=sum(list)/len(list)
+    print("Average temperature is:" + str(avg))
+    return avg
+def calc_min_max(list):
+    print("The minimum value is ")
+    print(min(list))
+    print("The maximum value is ")
+    print(max(list))
+    return min(list), max(list)
+def medianlist(list):
+    list.sort()
+    mid=len(list) // 2
+    res=(list[mid]+ list[~mid])/2
+    print("The median temp is " + str(res))
+    return res
 
 def get_user_input():
-    print("enter the values: ")
-    user_input = input()
-    user_input = user_input.split(",")
-    print(user_input)
-    float_user_input = list(map(float, user_input))
-    print(float_user_input)
-    return float_user_input
-
-def calc_average_temperature(final_user_inputs):
-    total = sum(final_user_inputs)
-    average = total/len(final_user_inputs)
-    print(average)
-    return average
-
-def calc_min_max_temperature(final_user_inputs):
-    min_temp = min(final_user_inputs)
-    max_temp = max(final_user_inputs)
-
-    print([int(min_temp), int(max_temp)])
-    return [int(min_temp), int(max_temp)]
+    print("Enter temp values separated by commas and press 'Enter'  e.g: 5,8,9,3")
+    tempstring=input()
+    tempstringList=tempstring.split(",")
+    print(tempstringList)
+    tempfloatlist=list(map(float,tempstringList))
+    print(str(tempfloatlist))
+    return tempfloatlist
 
 if __name__ == "__main__":
-    display_main_menu()
-    final_user_inputs = get_user_input()
-    calc_average_temperature(final_user_inputs)
-    calc_min_max_temperature(final_user_inputs)
+    main()
